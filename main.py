e405fb4ab205c9e9b68f6abbcff3df74cb3cdef7
nikitaMishr <nikitamishra9743@gmail.com>
2025-03-20 12:18:09 +0530

    importing all libraries

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 st.write("Hello World!") #for displaying text
 st.slider('Slide it: ',0,100)
